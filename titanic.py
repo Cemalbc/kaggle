f885ac50a4d149aa0f6c4c3cb936490fc0cd4c40
Bingchen Liu <47546131+Cemalbc@users.noreply.github.com>
2019-10-11 15:44:46 +0800

    Add files via upload
    stacking and ensembling

--- titanic.py
+++ titanic.py
@@ -9,6 +9,9 @@ import numpy as np
 import pandas_profiling
 import re
 
+import xgboost as xgb
+import seaborn as sns
+
 # visualization
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -26,6 +29,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import VotingClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
 
 global_path = 'D:/self-learn/kaggle/titanic'
 
@@ -33,10 +37,7 @@ df_train = pd.read_csv(global_path + '/train.csv')
 #pfr = pandas_profiling.ProfileReport(df_train)
 #pfr.to_file("D:/self-learn/kaggle/titanic/example.html")
 
-
 # Feature Engineering
-
-
 df_train[['Pclass','Survived']].groupby('Pclass').mean().sort_values(by='Survived', ascending=False)
 df_train[['Sex','Survived']].groupby('Sex').mean().sort_values(by='Survived', ascending=False)
 df_train[['SibSp','Survived']].groupby('SibSp').mean().sort_values(by='Survived', ascending=False)
@@ -118,7 +119,7 @@ def feature_engineer(df_train):
     age_std = df_train['Age'].std()
     age_null_count = df_train['Age'].isnull().sum()
     age_null_random = np.random.randint(age_mean-age_std,age_mean+age_std,size=age_null_count)
-    df_train['Age'][pd.isna(df_train['Age'])] = age_null_random
+    df_train.loc[df_train['Age'][pd.isna(df_train['Age'])].index,'Age'] = age_null_random
     df_train['Age'] = df_train['Age'].astype(int)
     # Transform age to categorical data
     df_train.loc[ df_train['Age'] <= 16, 'Age'] = 0
@@ -149,12 +150,101 @@ def feature_engineer(df_train):
 
 
 df_train = feature_engineer(df_train)
+df_test = pd.read_csv(global_path + '/test.csv')
+PassengerId = df_test['PassengerId']
+df_test = feature_engineer(df_test)
 
 
+# set parameters to kfolds
+ntrain = df_train.shape[0]
+ntest = df_test.shape[0]
+seed = 0
+nfolds = 5
+kf = KFold(nfolds,random_state=seed)
+
+# Class to extend sklearn classifier
+class SklearnHelper(object):
+    def __init__(self,clf,params=None):
+        if params == None:
+            params = {}
+        
+        self.clf = clf(**params)
+    
+    def train(self,x_train,y_train):
+        self.clf.fit(x_train,y_train)
+    
+    def predict(self,x):
+        return self.clf.predict(x)
+    
+    def fit(self,x,y):
+        self.clf.fit(x,y)
+        
+    def feature_importances(self,x,y):
+        return self.clf.fit(x,y).feature_importances_
+    
+    
+# fit the model and return the predictions for train and test
+def get_oof(clf,kf,nfolds,x_train,y_train,x_test):
+    oof_train = np.zeros((x_train.shape[0],))
+    oof_test = np.zeros((x_test.shape[0],))
+    oof_test_skf = np.empty((nfolds,x_test.shape[0]))
+    
+    i = 0
+    for train_index,test_index in kf.split(x_train):
+        x_tr = x_train[train_index]
+        y_tr = y_train[train_index]
+        x_te = x_train[test_index]
+        
+        clf.train(x_tr,y_tr)
+        
+        oof_train[test_index] = clf.predict(x_te)
+        oof_test_skf[i,:] = clf.predict(x_test)
+        i+=1
+    
+    oof_test[:] = np.array([i for i in map(lambda x:1 if x>=0.5 else 0,oof_test_skf.mean(axis=0))])
+    return oof_train.reshape(-1,1),oof_test.reshape(-1,1)
+    
+
+# Creat np arrays for train,test and target
+y_train =df_train['Survived'].ravel()
+df_train = df_train.drop(['Survived'],axis=1)
+x_train = df_train.values
+x_test = df_test.values 
+
+# Create different classifiers
+rf = SklearnHelper(RandomForestClassifier,params={'n_estimators':20,'max_depth':10,'min_samples_split':2})
+svc = SklearnHelper(SVC)
+lr = SklearnHelper(LogisticRegression)
+kn = SklearnHelper(KNeighborsClassifier)
+
+# Create our OOF train and test predictions. These base results will be used as new features
+rf_oof_train, rf_oof_test = get_oof(rf, kf,nfolds,x_train, y_train, x_test)
+svc_oof_train, svc_oof_test = get_oof(svc, kf,nfolds,x_train, y_train, x_test)
+lr_oof_train, lr_oof_test = get_oof(lr, kf,nfolds, x_train, y_train, x_test)
+kn_oof_train, kn_oof_test = get_oof(kn, kf,nfolds, x_train, y_train, x_test)
 
 
+x_train_2 = np.concatenate(( rf_oof_train, svc_oof_train, lr_oof_train, kn_oof_train), axis=1)
+x_test_2 = np.concatenate(( rf_oof_test, svc_oof_test, lr_oof_test, kn_oof_test), axis=1)
+
+gbm = xgb.XGBClassifier(
+ n_estimators= 2000,
+ max_depth= 4,
+ min_child_weight= 2,
+ gamma=0.9,                        
+ subsample=0.8,
+ colsample_bytree=0.8,
+ objective= 'binary:logistic',
+ nthread= -1,
+ scale_pos_weight=1).fit(x_train_2, y_train)
+predictions = gbm.predict(x_test_2)
 
 
+# Generate Submission File 
+StackingSubmission = pd.DataFrame({ 'PassengerId': PassengerId,'Survived': predictions })
+StackingSubmission.to_csv("StackingSubmission.csv", index=False)
+
+'''
 #df_train.dropna(axis=0,how='any',inplace=True)
 #df_train.fillna(-1,inplace=True)
 
@@ -216,3 +306,4 @@ test_predict = svc.predict(df_test)
 df_res = pd.DataFrame(data={'PassengerId':df_test['PassengerId'],'Survived':test_predict})
 
 df_res.to_csv(global_path + '/result00.csv',index=False)
+'''
